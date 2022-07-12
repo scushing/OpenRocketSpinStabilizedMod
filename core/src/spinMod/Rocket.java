cc7896a9ebecc09cc87a4dc85b26c5c9c9ab6e03
Stephen Cushing <73804392+scushing@users.noreply.github.com>
2022-07-11 18:02:52 -0700

    rocket class update

--- core/src/spinMod/Rocket.java
+++ core/src/spinMod/Rocket.java
@@ -2,7 +2,6 @@ package spinMod;
 
 public class Rocket {
 
-    TorqueCalculator NetTorqueCalculator;
     AngularMomentum xSpin;
     AngularMomentum ySpin;
     AngularMomentum zSpin;
@@ -10,7 +9,33 @@ public class Rocket {
     TorqueVector spin;
     TorqueVector drag;
     TorqueVector wind;
+    TorqueCalculator NetTorqueCalculator;
+
+    double mass;
+    double cgArm;
+    double dragCPArm;
+    double windCPArm;
+    double radius;
+    double baseSpin;
 
     double maxTime;
+
+    public Rocket(double mass, double cgArm, double radius, double baseSpin, double maxTime) {
+        this.mass = mass;
+        this.cgArm = cgArm;
+        this.radius = radius;
+        this.baseSpin = baseSpin;
+        this.maxTime = maxTime;
+
+        xSpin = new AngularMomentum(0, mass, radius, cgArm);
+        ySpin = new AngularMomentum(0, mass, radius, cgArm);
+        zSpin = new AngularMomentum(baseSpin, mass, radius);
+
+        orientation = new OrientationVector();
+        spin = new TorqueVector();
+        drag = new TorqueVector();
+        wind = new TorqueVector();
+        NetTorqueCalculator = new TorqueCalculator(spin, drag, wind);
+    }
 
 }
