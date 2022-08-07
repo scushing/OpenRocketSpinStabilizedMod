2d8c7d48c760cead9d4858104dd5ac8aa613cfbf
Daniel Johnson <danieldrdj01@gmail.com>
2022-08-07 04:30:45 -0700

    sideArea, topArea, dragCPArm, windCPArm methods implemented

--- core/src/spinMod/SpinSimulation/PredictedRocketGraphing.java
+++ core/src/spinMod/SpinSimulation/PredictedRocketGraphing.java
@@ -8,10 +8,7 @@ import net.sf.openrocket.document.OpenRocketDocument;
 import net.sf.openrocket.document.Simulation;
 import net.sf.openrocket.masscalc.MassCalculation;
 import net.sf.openrocket.motor.MotorConfiguration;
-import net.sf.openrocket.rocketcomponent.FlightConfiguration;
-import net.sf.openrocket.rocketcomponent.Rocket;
-import net.sf.openrocket.rocketcomponent.RocketComponent;
-import net.sf.openrocket.rocketcomponent.SymmetricComponent;
+import net.sf.openrocket.rocketcomponent.*;
 import net.sf.openrocket.util.Coordinate;
 import net.sf.openrocket.util.MathUtil;
 import spinMod.Gust;
@@ -43,10 +40,6 @@ public class PredictedRocketGraphing {
     private double thrust = 0;
     private double burnTime = 0;
     private double incrementSize = 0;
-
-
-
-
 
     public double getMass() {
         return mass;
@@ -160,21 +153,19 @@ public class PredictedRocketGraphing {
         incrementSize = value;
     }
 
-    private void retrieveMass(FlightConfiguration flightConfiguration){
+    private void setMass(FlightConfiguration flightConfiguration){
         Coordinate cg = MassCalculator.calculateLaunch(flightConfiguration).getCM();
         mass = cg.weight;
     }
 
-
-    private void retrieveCgArm(FlightConfiguration flightConfiguration){
+    private void setCgArm(FlightConfiguration flightConfiguration){
         double rocketLength = flightConfiguration.getLength();
         Coordinate cg = MassCalculator.calculateLaunch(flightConfiguration).getCM();
         double cgx = cg.x;
         cgArm = rocketLength - cgx;
     }
 
-
-    private void retrieveRadius(FlightConfiguration flightConfiguration){
+    private void setRadius(FlightConfiguration flightConfiguration){
         for (RocketComponent c : flightConfiguration.getCoreComponents()) {
             if (c instanceof SymmetricComponent) {
                 double d1 = ((SymmetricComponent) c).getForeRadius() * 2;
@@ -184,62 +175,208 @@ public class PredictedRocketGraphing {
         }
     }
 
-    private void retrieveTopDragCoefficient(FlightConfiguration flightConfiguration){
+    private void setTopDragCoefficient(FlightConfiguration flightConfiguration){
         WarningSet warnings = new WarningSet();
         FlightConditions conditions = new FlightConditions(flightConfiguration);
         AerodynamicCalculator aerodynamicCalculator = new BarrowmanCalculator();
         topDragCoefficient = aerodynamicCalculator.getAerodynamicForces(flightConfiguration, conditions,warnings).getCD();
     }
 
-    private void retrieveSideDragCoefficient(FlightConfiguration flightConfiguration){
+    private void setSideDragCoefficient(FlightConfiguration flightConfiguration){
         WarningSet warnings = new WarningSet();
         FlightConditions conditions = new FlightConditions(flightConfiguration);
         AerodynamicCalculator aerodynamicCalculator = new BarrowmanCalculator();
         sideDragCoefficient = aerodynamicCalculator.getAerodynamicForces(flightConfiguration, conditions,warnings).getCDaxial();
     }
 
-    private void retrieveDragCPArm(FlightConfiguration flightConfiguration){
+    //Does not currently adjust for overlapping SETS of fins
+    private void setSideArea(FlightConfiguration flightConfiguration){
+        double finArea = 0;
+        double currentArea = 0;
+        ArrayList<RocketComponent> rocketComponents = flightConfiguration.getCoreComponents();
+        for(RocketComponent component : rocketComponents){
+            if (component instanceof SymmetricComponent) {
+                double largestRadius = 0;
+                double r1 = ((SymmetricComponent) component).getForeRadius();
+                double r2 = ((SymmetricComponent) component).getAftRadius();
+                if (largestRadius < r1 || largestRadius < r2) {
+                    largestRadius = MathUtil.max(largestRadius, r1, r2);
+                }
+                //Does not account yet for two separate width sections on one piece.
+                currentArea += largestRadius * 2 * component.getLength();
+            } else if (component instanceof FinSet) {
+                double singleFinArea = ((FinSet) component).getPlanformArea();
+                double currentFinCount = ((FinSet) component).getFinCount();
+                double currentCantAngle = ((FinSet) component).getCantAngle();
+                double currentFinThickness = ((FinSet) component).getThickness();
+                double currentFinLength = 0;
+                double totalFinArea;
+
+                if (component instanceof TrapezoidFinSet) {
+                    currentFinLength = ((TrapezoidFinSet) component).getHeight();
+                } else if(component instanceof EllipticalFinSet) {
+                    currentFinLength = ((EllipticalFinSet) component).getHeight();
+                } else if(component instanceof FreeformFinSet) {
+                    Coordinate[] finPoints = ((FreeformFinSet) component).getFinPoints();
+                    for (Coordinate finPoint : finPoints) {
+                        currentFinLength = MathUtil.max(currentFinLength, finPoint.y);
+                    }
+                }
+
+                if(((FinSet) component).getCantAngle() == 0){
+                    totalFinArea = (singleFinArea);
+                } else {
+                    totalFinArea = ((singleFinArea * Math.cos(currentCantAngle))
+                            + ((currentFinLength * currentFinThickness) * Math.cos(Math.PI - currentCantAngle)));
+                }
+                if (currentFinCount % 2 == 0){
+                    totalFinArea *= 2;
+                } else {
+                    totalFinArea *= 2 * Math.sin((2 * Math.PI) / currentFinCount);
+                }
+                finArea += totalFinArea;
+            }
+        }
+        sideArea = finArea + currentArea;
+    }
+
+    //Does not currently adjust for overlapping SETS of fins
+    private void setTopArea(FlightConfiguration flightConfiguration){
+        ArrayList<RocketComponent> rocketComponents = flightConfiguration.getCoreComponents();
+        double largestRadius = 0;
+        double finArea = 0;
+        for(RocketComponent component : rocketComponents){
+            if (component instanceof SymmetricComponent) {
+                double r1 = ((SymmetricComponent) component).getForeRadius();
+                double r2 = ((SymmetricComponent) component).getAftRadius();
+                if (largestRadius < r1 || largestRadius < r2) {
+                    largestRadius = MathUtil.max(largestRadius, r1, r2);
+                }
+            } else if (component instanceof FinSet) {
+                double currentFinArea = ((FinSet) component).getPlanformArea();
+                double currentFinCount = ((FinSet) component).getFinCount();
+                double currentCantAngle = ((FinSet) component).getCantAngle();
+                double currentFinThickness = ((FinSet) component).getThickness();
+                double currentFinLength = 0;
+
+
+                if (component instanceof TrapezoidFinSet) {
+                    currentFinLength = ((TrapezoidFinSet) component).getHeight();
+                } else if(component instanceof EllipticalFinSet) {
+                    currentFinLength = ((EllipticalFinSet) component).getHeight();
+                } else if(component instanceof FreeformFinSet) {
+                    Coordinate[] finPoints = ((FreeformFinSet) component).getFinPoints();
+                    for (Coordinate finPoint : finPoints) {
+                        currentFinLength = MathUtil.max(currentFinLength, finPoint.y);
+                    }
+                }
+
+                if(((FinSet) component).getCantAngle() == 0){
+                    finArea += currentFinCount * (currentFinArea);
+                } else {
+                    finArea += currentFinCount * ((currentFinArea * Math.cos(Math.PI - currentCantAngle))
+                            + ((currentFinLength * currentFinThickness) * Math.cos(currentCantAngle)));
+                }
+            }
+        }
+        sideArea = (MathUtil.pow2(largestRadius) * Math.PI) + finArea;
+    }
+
+    private void setDragCPArm(FlightConfiguration flightConfiguration){
         double rocketLength = flightConfiguration.getLength();
         WarningSet warnings = new WarningSet();
         FlightConditions conditions = new FlightConditions(flightConfiguration);
         AerodynamicCalculator aerodynamicCalculator = new BarrowmanCalculator();
-        //aerodynamicCalculator.getAerodynamicForces(curConfig, conditions, warnings);
 
         Coordinate cp = aerodynamicCalculator.getWorstCP(flightConfiguration, conditions, warnings);
         double cpx = cp.x;
         dragCPArm = rocketLength - cpx;
-        System.out.println();
     }
 
-    private void retrieveSideArea(FlightConfiguration flightConfiguration){
+    //Adjustments still need to be made to the fin section of this code
+    private void setWindCPArm(FlightConfiguration flightConfiguration){
+        double currentIntegrationCenter = 0;
+        double previousCombinedArea = 0;
 
-    }
+        double currentComponentArea;
+        double currentComponentAreaCenter = 0;
 
-    private void retrieveTopArea(FlightConfiguration flightConfiguration){
+
         ArrayList<RocketComponent> rocketComponents = flightConfiguration.getCoreComponents();
-        for(RocketComponent component : rocketComponents){
+        for(RocketComponent component : rocketComponents) {
+            double largestRadius = 0;
             if (component instanceof SymmetricComponent) {
-                double d1 = ((SymmetricComponent) component).getForeRadius() * 2;
-                double d2 = ((SymmetricComponent) component).getAftRadius() * 2;
-                radius = MathUtil.max(radius, d1, d2) / 2;
+                double r1 = ((SymmetricComponent) component).getForeRadius();
+                double r2 = ((SymmetricComponent) component).getAftRadius();
+                if (largestRadius < r1 || largestRadius < r2) {
+                    largestRadius = MathUtil.max(largestRadius, r1, r2);
+                }
             }
+            if (component instanceof NoseCone) {
+                currentComponentArea = component.getLength() * largestRadius;
+                currentComponentAreaCenter = 2 * component.getLength() / 3;
+                if(currentIntegrationCenter == 0) {
+                    currentIntegrationCenter += currentComponentAreaCenter;
+                } else {
+                    currentIntegrationCenter += currentComponentAreaCenter * currentComponentArea / previousCombinedArea;
+                }
+                previousCombinedArea += currentComponentArea;
+            } else if (component instanceof SymmetricComponent){
+                currentComponentArea = (2 * largestRadius * component.getLength());
+                currentComponentAreaCenter = component.getLength() / 2;
+                if(currentIntegrationCenter == 0) {
+                    currentIntegrationCenter += currentComponentAreaCenter;
+                } else {
+                    currentIntegrationCenter += currentComponentAreaCenter * currentComponentArea / previousCombinedArea;
+                }
+                previousCombinedArea += currentComponentArea;
+            } else if (component instanceof FinSet) {
+                double singleFinArea = ((FinSet) component).getPlanformArea();
+                double currentFinCount = ((FinSet) component).getFinCount();
+                double currentCantAngle = ((FinSet) component).getCantAngle();
+                double currentFinThickness = ((FinSet) component).getThickness();
+                double currentFinLength = 0;
+                double totalFinArea;
+
+                if (component instanceof TrapezoidFinSet) {
+                    currentFinLength = ((TrapezoidFinSet) component).getHeight();
+                } else if(component instanceof EllipticalFinSet) {
+                    currentFinLength = ((EllipticalFinSet) component).getHeight();
+                } else if(component instanceof FreeformFinSet) {
+                    Coordinate[] finPoints = ((FreeformFinSet) component).getFinPoints();
+                    for (Coordinate finPoint : finPoints) {
+                        currentFinLength = MathUtil.max(currentFinLength, finPoint.y);
+                    }
+                }
+
+                if(((FinSet) component).getCantAngle() == 0){
+                    totalFinArea = (singleFinArea);
+                } else {
+                    totalFinArea = ((singleFinArea * Math.cos(currentCantAngle))
+                            + ((currentFinLength * currentFinThickness) * Math.cos(Math.PI - currentCantAngle)));
+                }
+                if (currentFinCount % 2 == 0){
+                    totalFinArea *= 2;
+                } else {
+                    totalFinArea *= 2 * Math.sin((2 * Math.PI) / currentFinCount);
+                }
+                currentComponentArea = totalFinArea;
+                currentComponentAreaCenter = component.getLength() / 2;
+                currentIntegrationCenter += (component.getAxialOffset()) + currentComponentAreaCenter * currentComponentArea / previousCombinedArea;
+                previousCombinedArea += currentComponentArea;
+            }
+
         }
+
+        windCPArm = flightConfiguration.getLength() - currentIntegrationCenter;
     }
 
-    private void retrieveDragCPArm(){
-
-    }
-
-    private void retrieveWindCPArm(){
-
-    }
-
-    private void retrieveThrust(FlightConfiguration flightConfiguration){
+    private void setThrust(FlightConfiguration flightConfiguration){
         MotorConfiguration motorConfiguration = flightConfiguration.getAllMotors().iterator().next();
         thrust = motorConfiguration.getMotor().getAverageThrustEstimate();
     }
 
-    private void retrieveBurnTime(FlightConfiguration flightConfiguration){
+    private void setBurnTime(FlightConfiguration flightConfiguration){
         MotorConfiguration motorConfiguration = flightConfiguration.getAllMotors().iterator().next();
         burnTime = motorConfiguration.getMotor().getBurnTimeEstimate();
     }
@@ -249,22 +386,29 @@ public class PredictedRocketGraphing {
         FlightConfiguration curConfig = sim.getActiveConfiguration();
 
         // Use for application
-        retrieveMass(curConfig);
-        retrieveCgArm(curConfig);
-        retrieveRadius(curConfig);
+        setMass(curConfig);
+        setCgArm(curConfig);
+        setRadius(curConfig);
         setBaseSpin(0);
         setAirDensity(1.33);
-        retrieveTopDragCoefficient(curConfig);
-        retrieveSideDragCoefficient(curConfig);
-        setTopDragCoefficient(0.2);
-        setSideDragCoefficient(0.3);
-        setSideArea(0.0016); // Cross Sectional Area
-        setTopArea(0.000075); // Biggest Diameter + Fin top area * fin amount
-        retrieveDragCPArm(curConfig);
-        //setDragCPArm(0.07); // Bottom to Center of pressure
-        setWindCPArm(0.08); // Cross Sectional Area - Integrate & Find midpoint
-        retrieveThrust(curConfig);
-        retrieveBurnTime(curConfig);
+        setTopDragCoefficient(curConfig);
+        setSideDragCoefficient(curConfig);
+        setTopDragCoefficient(curConfig);
+        //setTopDragCoefficient(0.2);
+        //SideDragCoefficient is currently returns the same as value as the TopDragCoefficient
+        //I would recommend putting in a value manually for testing if this does not seem correct
+        setSideDragCoefficient(curConfig);
+        //setSideDragCoefficient(0.3);
+        setSideArea(curConfig); // Cross Sectional Area
+        //setSideArea(0.0016);
+        setTopArea(curConfig); // Biggest Diameter + Fin top area * fin amount
+        //setTopArea(0.000075);
+        setDragCPArm(curConfig); // Bottom to Center of pressure
+        //setDragCPArm(0.07);
+        setWindCPArm(curConfig); // Cross Sectional Area - Integrate & Find midpoint
+        //setWindCPArm(0.08);
+        setThrust(curConfig);
+        setBurnTime(curConfig);
         setIncrementSize(0.001);
 
 
@@ -292,10 +436,11 @@ public class PredictedRocketGraphing {
 
         retrieveData(sim);
 
-        StabilitySim stabilitySim = new StabilitySim(this.mass, cgArm, radius, baseSpin, airDensity, topDragCoefficient,
-                sideDragCoefficient, sideArea, topArea, dragCPArm, windCPArm, thrust, burnTime, incrementSize);
-
+        StabilitySim stabilitySim = new StabilitySim(getMass(), getCgArm(), getRadius(), getBaseSpin(),
+                getAirDensity(), getTopDragCoefficient(), getSideDragCoefficient(), getSideArea(),
+                getTopArea(), getDragCPArm(), getWindCPArm(), getThrust(), getBurnTime(), getIncrementSize());
         //Use for manual testing
+
         /*
         StabilitySim stabilitySim = new StabilitySim(0.5, 0.13, 0.023, 200, 1.33, 0.2,
                 0.3, 0.0016, 0.000075, 0.07, 0.08, 44, 1, 0.001);
